5bda33c597583aca0685c65020dc0131364cbc78
Arek W <arek01@gmail.com>
2014-03-31 21:29:42 +1100

    make total stand out

--- test.js
+++ test.js
@@ -15,11 +15,13 @@ xml += '</box>';
 
 var xml = fs.readFileSync('resp.xml', 'ascii');
 
-console.time("total");
+console.time("## TOTAL");
 expat.convert(xml, { a: 2, b: 3 }, function (err, data, jsonStr) {
+  console.time("json.parse");
   var json = JSON.parse(jsonStr);
+  console.timeEnd("json.parse");
 
-  console.timeEnd("total");
+  console.timeEnd("## TOTAL");
 
   fs.writeFileSync("out2.json", JSON.stringify(json, null, 2));
 });
